93d4f8c1e62f8d20281a93c7c41fd82be0ed9acf
BharathRoyals <sbharath2703@gmail.com>
2023-05-08 17:10:15 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from app.forms import *
+from django.http import HttpResponseRedirect
 from django.http import HttpResponse
+from django.urls import reverse
+from django.contrib.auth import authenticate,login,logout
 from django.core.mail import send_mail
+from django.contrib.auth.decorators import login_required
 def home(request):
     if request.session.get('username'):
         username=request.session.get('username')
@@ -49,3 +53,23 @@ def registrations(request):
 
 
     return render(request,'registrations.html',d)
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('Invalid username or password')
+        
+    return render(request,'user_login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
